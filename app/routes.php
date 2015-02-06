28ea7efa99b5a0417004894efedf4e883ed6f707
Iaroslav <beststrelok@gmail.com>
2015-02-06 20:38:43 +0200

    search fix

--- app/routes.php
+++ app/routes.php
@@ -18,7 +18,7 @@ Route::post('/registration', 'MainController@registration');
 Route::post('/feedback', 'MainController@feedback');
 Route::get('/order/{item_title}/{item_code}', 'MainController@order_page');
 Route::post('/order', 'MainController@order');
-Route::get('/search/{query}', 'MainController@search');
+Route::get('/search/{query?}', 'MainController@search');
 Route::get('/logout', 'MainController@logout');
 
 
