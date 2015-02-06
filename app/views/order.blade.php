e376850b7cdb0fcd5558a98ecf0e0958b162748a
Bogdan Kootz <bodyakootz@gmail.com>
2015-02-06 19:58:45 +0200

    Header+FA

---
+++ app/views/order.blade.php
@@ -0,0 +1,41 @@
+@section('header') 
+	<header>
+		<hr class="header_top_hr">
+		<p class="header_phone">8 (812) 982 33 54</p>
+		<p class="header_phone">8 (812) 982 33 54</p>
+		<img src="" alt="" class="logo_header">
+		<div class="header_description">
+			<h1 class="header_descriprion_heading">OOO "ЗИП Общепит"</h1>
+			<p class="header_description_text">Lorem ipsum dolor sit amet,<br>
+			consectetur adipisicing elit. Obcaecati, saepe.</p>
+			<div class="btn-group register" role="group" aria-label="...">
+			  <button type="button" class="btn btn-default">Войти</button>
+			  <button type="button" class="btn btn-default">Зарегестрироваться</button>
+			  <p class="navbar-text">Вы вошли как(some data)</p>
+			</div>
+			<form class="form-inline" role="search">
+		         <div class="input-group">
+      				<div class="input-group-addon"><i class="fa fa-search"></i></div>
+      				<input type="text" class="form-control" id="search" placeholder="Поиск по каталогу">
+      			</div>	
+		    </form>
+		    <nav class="navbar navbar-default">
+			    <!-- Collect the nav links, forms, and other content for toggling -->
+			    <div class="collapse navbar-collapse">
+					<ul class="nav navbar-nav">
+					<li class="active"><a href="#">Каталог <span class="sr-only">(current)</span></a></li>
+					<li><a href="#">Прайс-лист</a></li>
+					<li class="divider"></li>
+					<li><a href="#">Доставка</a></li>
+					<li class="divider"></li>
+					<li><a href="#">Специальные предложения</a></li>
+					<li class="divider"></li>
+					<li><a href="#">О нас</a></li>
+					<li class="divider"></li>
+					<li><a href="#">Контакты</a></li>
+					<li class="divider"></li>
+					</ul>
+			    </div><!-- /.navbar-collapse -->
+			</nav>
+	</header>
+@stop
