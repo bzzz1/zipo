e0214dfbc6fe8d59c6855561aa6b2d0034238f42
Bogdan Kootz <bodyakootz@gmail.com>
2015-02-11 12:19:51 +0200

    markup 0.98

--- app/views/footer.blade.php
+++ app/views/footer.blade.php
@@ -2,12 +2,12 @@
 	<div class="container">
 		<img src="" alt="" class="footer_logo">
 		<ul class="nav navbar-nav">
-			<li class="active"><a href="#" class="@if ($env == '/') selected @endif">Каталог</a></li>
-			<li><a href="#" class="@if ($env == '/price') selected @endif">Прайс-лист</a></li>
-			<li><a href="#" class="@if ($env == '/delivery') selected @endif">Доставка</a></li>
-			<li><a href="#" class="@if ($env == '/specials') selected @endif">Специальные предложения</a></li>
-			<li><a href="#" class="@if ($env == '/about') selected @endif">О нас</a></li>
-			<li><a href="#" class="@if ($env == '/contacts') selected @endif">Контакты</a></li>
+			<li><a href="#">Каталог</a></li>
+			<li><a href="#">Прайс-лист</a></li>
+			<li><a href="#">Доставка</a></li>
+			<li><a href="#">Специальные предложения</a></li>
+			<li><a href="#">О нас</a></li>
+			<li><a href="#">Контакты</a></li>
 		</ul>
 		<p class="footer_description">Description of company(very short)</p>
 		<p class="footer_copy"><i class="fa fa-copyright"></i>2015. "Зип Общепит" All reghts reserved</p>
