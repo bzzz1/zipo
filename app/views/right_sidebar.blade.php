12659a7efa8f08bbf70a097db61844f8cfbc09da
Bogdan Kootz <bodyakootz@gmail.com>
2015-02-11 13:22:44 +0200

    markup 0.991

--- app/views/right_sidebar.blade.php
+++ app/views/right_sidebar.blade.php
@@ -5,9 +5,9 @@
       			<h3 class="articles_heading">Новости</h3>
                 @foreach ($articles as $article)
           			<div class="article">
-          				<a href="/articles/{{App::make('HelperController')->url_slug(\"$article->title?article_id=$article->article_id" class="article_link"><img src="{{$article->photo}}" alt="{{$article->title}}" class="article_minimage"></a>
-          				<p class="article_date">{{--$article->time--}}</p>
-          				<a href="/articles/$article->title?article_id=$article->article_id" class="article_title">{{--$article->title--}}</a>
+          				<a href='/articles/{{App::make("HelperController")->url_slug("$article->title")}}?article_id={{$article->article_id}}' class="article_link"><img src="{{$article->photo}}" alt="{{$article->title}}" class="article_minimage"></a>
+          				<p class="article_date">{{$article->time}}</p>
+          				<a href='/articles/{{App::make("HelperController")->url_slug("$article->title")}}?article_id={{$article->article_id}}' class="article_title">{{$article->title}}</a>
           			</div>
                 @endforeach
       			<a href="/articles" class="articles_all_link">Все новости >> </a>
