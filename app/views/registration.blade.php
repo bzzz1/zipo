5a2568d9b86729db51e1f61c27eb9d2b1009d73c
Bogdan Kootz <bodyakootz@gmail.com>
2015-02-11 13:11:55 +0200

    markup 0.990

--- app/views/registration.blade.php
+++ app/views/registration.blade.php
@@ -9,41 +9,41 @@
 		<h2 class="registration_heading">Регистрация</h2>
 		<hr class="main_hr">
 		{{ Form::open(['url'=>'/registration', 'method'=>'POST', 'class'=>'register_form']) }}
-				<table>
-					<tr>
-						<td>{{ Form::label('name', 'Имя: ', ['class'=>'main_label']) }}</td>
-						<td>{{ Form::text('name', null, ['class'=>'change_input', 'required']) }}</td>
-					</tr>
-					<tr>
-						<td>{{ Form::label('surname', 'Фамилия: ', ['class'=>'main_label']) }}</td>
-						<td>{{ Form::textarea('surname', null, ['class'=>'change_input', 'required']) }}</td>
-					</tr>
-					<tr>
-						<td>{{ Form::label('company', 'Компания: ', ['class'=>'main_label']) }}</td>
-						<td>{{ Form::text('company', null, ['class'=>'change_input change_input_code']) }}</td>
-					</tr>
-					<tr>
-						<td>{{ Form::label('email', 'E-Mail: ', ['class'=>'main_label']) }}</td>
-						<td>{{ Form::text('email', null, ['class'=>'change_input change_input_code', 'required']) }}</td>
-					</tr>
-					<tr>
-						<td>{{ Form::label('phone', 'Телефон: ', ['class'=>'main_label']) }}</td>
-						<td>{{ Form::text('phone', null, ['class'=>'change_input change_input_code', 'required']) }}</td>
-					</tr>
-					<tr>
-						<td>{{ Form::label('activity', 'Род деятельности: ', ['class'=>'main_label']) }}</td>
-						<td>{{ Form::select('activity',['Технолог' => 'Технолог','Владелец' => 'Владелец', 'Инжинер' => 'Инженер'], ['class'=>'change_input change_input_code']) }}</td>
-					</tr>
-					<tr>
-						<td>{{ Form::label('password', 'Пароль: ', ['class'=>'main_label']) }}</td>
-						<td>{{ Form::password('password', ['class'=>'change_input change_input_short','required']) }}</td>
-					</tr>
-					<tr>
-						<td>{{ Form::label('confirm', 'Подтверждение пароля: ', ['class'=>'main_label']) }}</td>
-						<td>{{ Form::password('confirm', ['class'=>'change_input change_input_code', 'required']) }}</td>
-					</tr>
-				</table>
-				{{ Form::submit('Регистрация', ['class'=>'submit_field save_button']) }}
-			{{ Form::close() }}
+			<table>
+				<tr>
+					<td>{{ Form::label('name', 'Имя: ', ['class'=>'main_label']) }}</td>
+					<td>{{ Form::text('name', null, ['class'=>'change_input', 'required']) }}</td>
+				</tr>
+				<tr>
+					<td>{{ Form::label('surname', 'Фамилия: ', ['class'=>'main_label']) }}</td>
+					<td>{{ Form::textarea('surname', null, ['class'=>'change_input', 'required']) }}</td>
+				</tr>
+				<tr>
+					<td>{{ Form::label('company', 'Компания: ', ['class'=>'main_label']) }}</td>
+					<td>{{ Form::text('company', null, ['class'=>'change_input change_input_code']) }}</td>
+				</tr>
+				<tr>
+					<td>{{ Form::label('email', 'E-Mail: ', ['class'=>'main_label']) }}</td>
+					<td>{{ Form::text('email', null, ['class'=>'change_input change_input_code', 'required']) }}</td>
+				</tr>
+				<tr>
+					<td>{{ Form::label('phone', 'Телефон: ', ['class'=>'main_label']) }}</td>
+					<td>{{ Form::text('phone', null, ['class'=>'change_input change_input_code', 'required']) }}</td>
+				</tr>
+				<tr>
+					<td>{{ Form::label('activity', 'Род деятельности: ', ['class'=>'main_label']) }}</td>
+					<td>{{ Form::select('activity',['Технолог' => 'Технолог','Владелец' => 'Владелец', 'Инжинер' => 'Инженер'], ['class'=>'change_input change_input_code']) }}</td>
+				</tr>
+				<tr>
+					<td>{{ Form::label('password', 'Пароль: ', ['class'=>'main_label']) }}</td>
+					<td>{{ Form::password('password', ['class'=>'change_input change_input_short','required']) }}</td>
+				</tr>
+				<tr>
+					<td>{{ Form::label('confirm', 'Подтверждение пароля: ', ['class'=>'main_label']) }}</td>
+					<td>{{ Form::password('confirm', ['class'=>'change_input change_input_code', 'required']) }}</td>
+				</tr>
+			</table>
+			{{ Form::submit('Регистрация', ['class'=>'submit_field save_button']) }}
+		{{ Form::close() }}
 	</div>
 @stop
--- app/views/right_sidebar.blade.php
+++ app/views/right_sidebar.blade.php
@@ -5,7 +5,7 @@
       			<h3 class="articles_heading">Новости</h3>
                 @foreach ($articles as $article)
           			<div class="article">
-          				<a href="/articles/$article->title?article_id=$article->article_id" class="article_link"><img src="$article->photo" alt="$article->title" class="article_minimage"></a>
+          				<a href="/articles/{{App::make('HelperController')->url_slug(\"$article->title?article_id=$article->article_id" class="article_link"><img src="{{$article->photo}}" alt="{{$article->title}}" class="article_minimage"></a>
           				<p class="article_date">{{--$article->time--}}</p>
           				<a href="/articles/$article->title?article_id=$article->article_id" class="article_title">{{--$article->title--}}</a>
           			</div>
