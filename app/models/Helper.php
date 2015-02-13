58c1b6ca7f63a76d09b72eeff72ca4495efb69f5
Iaroslav <beststrelok@gmail.com>
2015-02-13 19:29:03 +0200

    update url_slug

--- app/models/Helper.php
+++ app/models/Helper.php
@@ -72,7 +72,7 @@ class Helper {
 		return $result; 
 	}
 
-	public static function url_slug($str, $options = array()) {
+	private static function url_slug_chunk($str, $options = array()) {
 		// URL SLUG https://gist.github.com/sgmurphy/3098978
 		// Make sure string is in UTF-8 and strip invalid UTF-8 characters
 		$str = mb_convert_encoding((string)$str, 'UTF-8', mb_list_encodings());
@@ -81,7 +81,11 @@ class Helper {
 			'delimiter' => '_',
 			'limit' => null,
 			'lowercase' => true,
-			'replacements' => array(),
+			'replacements' => array(
+				'/\?/i' => '\_',
+				'/\//i' => '\_',
+				'/\^/i' => '\_',
+			),
 			'transliterate' => true,
 		);
 
@@ -177,6 +181,77 @@ class Helper {
 		
 		return $options['lowercase'] ? mb_strtolower($str, 'UTF-8') : $str;
 	}
+
+	public static function url_slug(array $arr) {
+		$url = '';
+		foreach ($arr as $chunk) {
+			if ($chunk == '/') {
+				$url .= $chunk;
+			} else {
+				$url .= static::url_slug_chunk($chunk);
+			}
+		}
+		
+		dd($url);
+		return $url;
+	}
+
+	/*------------------------------------------------
+	| URL SLUG USAGE
+	------------------------------------------------*/
+	// include('url_slug.php');
+	// header('Content-type: text/plain; charset=utf-8');
+
+	// // Basic usage
+	// echo "This is an example string. Nothing fancy." . "\n";
+	// echo url_slug("This is an example string. Nothing fancy.") . "\n\n";
+
+	// // Example using French with unwanted characters ('?)
+	// echo "Qu'en est-il français? Ça marche alors?" . "\n";
+	// echo url_slug("Qu'en est-il français? Ça marche alors?") . "\n\n";
+
+	// // Example using transliteration
+	// echo "Что делать, если я не хочу, UTF-8?" . "\n";
+	// echo url_slug("Что делать, если я не хочу, UTF-8?", array('transliterate' => true)) . "\n\n";
+
+	// // Example using transliteration on an unsupported language
+	// echo "מה אם אני לא רוצה UTF-8 תווים?" . "\n";
+	// echo url_slug("מה אם אני לא רוצה UTF-8 תווים?", array('transliterate' => true)) . "\n\n";
+
+	// // Some other options
+	// echo "This is an Example String. What's Going to Happen to Me?" . "\n";
+	// echo url_slug(
+	// 	"This is an Example String. What's Going to Happen to Me?", 
+	// 	array(
+	// 		'delimiter' => '_',
+	// 		'limit' => 40,
+	// 		'lowercase' => false,
+	// 		'replacements' => array(
+	// 			'/\b(an)\b/i' => 'a',
+	// 			'/\b(example)\b/i' => 'Test'
+	// 		)
+	// 	)
+	// );
+
+	// /*
+	// Output:
+
+	// This is an example string. Nothing fancy.
+	// this-is-an-example-string-nothing-fancy
+
+	// Qu'en est-il français? Ça marche alors?
+	// qu-en-est-il-français-ça-marche-alors
+
+	// Что делать, если я не хочу, UTF-8?
+	// chto-delat-esli-ya-ne-hochu-utf-8
+
+	// מה אם אני לא רוצה UTF-8 תווים?
+	// מה-אם-אני-לא-רוצה-utf-8-תווים
+
+	// This is an Example String. What's Going to Happen to Me?
+	// This_is_a_Test_String_What_s_Going_to_Ha
+	// */
+	///*----------------------------------------------*/
 
 	public static function getNormal($str) {
 		$end = strlen($str)-3;
