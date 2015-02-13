b2d45befab5094f8b143f524099439e8d973a739
Bogdan Kootz <bodyakootz@gmail.com>
2015-02-13 19:29:48 +0200

    markup 1.0002

--- app/views/header.blade.php
+++ app/views/header.blade.php
@@ -12,29 +12,32 @@
 			<div class="header_description">
 				<h1 class="header_descriprion_heading">OOO "ЗИП Общепит"</h1><br>
 				<p class="header_description_text">Lorem ipsum dolor sit amet,<br>
-				consectetur adipisicing elit. Obcaecati, saepe.</p>
+				consectetur adipisicing elit. Obcaecati, saepe. <br>Lorem ipsum dolor <br> sit amet.</p>
 			</div>	
-			@if (Auth::check())
-				<p class="header_login">Вы вошли как {{--$user->email--}}</p>
-			@else 
-				<div class="btn-group register" role="group" aria-label="...">
-					<button type="button" class="btn btn-default">Войти</button>
-					<a href="/registration"><button type="button" class="btn btn-default">Регистрация</button></a>
-				</div>
-				<div class="header_login">
-					@if (isset($error))
-						<p class="error">Error</p>
-					@endif	
-					{{ Form::open(['url'=>'/user_login', 'method'=>'POST', 'class'=>'login_form']) }}
-						{{ Form::text('email', null, ['class'=>'login_input', 'required', 'placeholder'=>"Ваш e-mail"]) }}
-						{{ Form::password('password', ['class'=>'login_input', 'required', 'placeholder'=>"Ваш пароль"]) }}
-						{{ Form::submit('Войти', ['class'=>'submit_field save_button']) }}
-					{{ Form::close() }}
-				</div>	
-			@endif
-		    {{ Form::open(array('url' => "/search", 'method' => 'GET', 'class'=>'form-inline header_search')) }}
-				{{ Form::text('query', null, ['placeholder'=>"Поиск по каталогу", 'class'=>'form-control', 'id' =>'search']) }} 
-			{{ Form::close() }}
+			<div class="header_reg_adn_log">
+				@if (Auth::check())
+					<p class="header_login">Вы вошли как {{--$user->email--}}</p>
+				@else 
+					<div class="btn-group btn-group-lg register" role="group" aria-label="reg">
+						<button type="button" class="btn btn-default">Войти</button>
+						<button type="button" class="btn btn-default reg_button"><a href="/registration" class="register_a">Регистрация</a></button>
+					</div>
+					<div class="header_login">
+						@if (isset($error))
+							<p class="error">Error</p>
+						@endif	
+						{{ Form::open(['url'=>'/user_login', 'method'=>'POST', 'class'=>'login_form input-group']) }}
+							{{ Form::text('email', null, ['class'=>'login_input', 'required', 'placeholder'=>"Ваш e-mail", 'class'=>'login_form_input']) }}
+							{{ Form::password('password', ['class'=>'login_input', 'required', 'placeholder'=>"Ваш пароль", 'class'=>'login_form_input']) }}
+							{{ Form::submit('Войти', ['class'=>'submit_field']) }}
+						{{ Form::close() }}
+					</div>	
+				@endif
+			    {{ Form::open(array('url' => "/search", 'method' => 'GET', 'class'=>'header_search')) }}
+					<!-- <span class="input-group-addon" id="sizing-addon1">@</span> -->
+					{{ Form::text('query', null, ['placeholder'=>"Поиск по каталогу", 'class'=>'form-control', 'id' =>'search']) }} 
+				{{ Form::close() }}
+			</div>	
 		    <nav class="navbar navbar-inverse">
 				<ul class="nav navbar-nav">
 					<li class="@if ($env == 'catalog' || $env == 'byproducer' || $env == 'search') active @endif"><a href="/">Каталог</a></li>
