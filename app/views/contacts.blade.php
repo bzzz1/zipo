da5ec01ed1e93d916b87a755cb1d312164b368b3
Bogdan Kootz <bodyakootz@gmail.com>
2015-02-17 19:32:08 +0200

    markup 1.0008

--- app/views/contacts.blade.php
+++ app/views/contacts.blade.php
@@ -24,41 +24,40 @@
 			<p class="about_text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, recusandae.</p>
 			<p class="about_text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat, unde!</p>
 		</div>
-	<p class="conacts_subheading">Если у вас остались вопросы - напишите нам.</p>
-	<div class="contacts_contact_form" id="contact_sorm_ancher">
-		{{ Form::open(['url'=>'/feedback', 'method'=>'POST', 'class'=>'item_form admin_info_form']) }}
-			<table>
-				<tr>
-					<td>{{ Form::label('name', 'Имя: ', ['class'=>'main_label']) }}</td>
-					<td>{{ Form::text('name', null, ['class'=>'change_input', 'required']) }}</td>
-				</tr>
-				<tr>
-					<td>{{ Form::label('company', 'Компания: ', ['class'=>'main_label']) }}</td>
-					<td>{{ Form::text('company', null, ['class'=>'change_input']) }}</td>
-				</tr>
-				<tr>
-					<td>{{ Form::label('phone', 'Контактный телефон: ', ['class'=>'main_label']) }}</td>
-					<td>{{ Form::text('phone', null, ['class'=>'change_input']) }}</td>
-				</tr>
-				<tr>
-					<td>{{ Form::label('email', 'E-mail: ', ['class'=>'main_label']) }}</td>
-					<td>{{ Form::text('email', null, ['class'=>'change_input', 'required']) }}</td>
-				</tr>
-				<tr>
-					<td>{{ Form::label('theme', 'Тема письма: ', ['class'=>'main_label']) }}</td>
-					<td>{{ Form::text('theme', null, ['class'=>'change_input']) }}</td>
-				</tr>
-				<tr>
-					<td>{{ Form::label('body', 'Текст: ', ['class'=>'main_label']) }}</td>
-					<td>{{ Form::text('body', null, ['class'=>'change_input long_textarea', 'required']) }}</td>
-				</tr>
-			</table>
-			{{ Form::submit('Отправить', ['class'=>'submit_field save_button']) }} 
-		{{ Form::close() }}
-	</div>
-	</div>
-	<div id="map-container"></div>
-	<script>    
+		<p class="conacts_subheading">Если у вас остались вопросы - напишите нам.</p>
+		<div class="contacts_contact_form" id="contact_sorm_ancher">
+			{{ Form::open(['url'=>'/feedback', 'method'=>'POST', 'class'=>'item_form admin_info_form']) }}
+				<table class="contacts_form_table">
+					<tr>
+						<td>{{ Form::label('name', 'Имя: ', ['class'=>'main_label']) }}</td>
+						<td>{{ Form::text('name', null, ['class'=>'change_input_contacts', 'required']) }}</td>
+					</tr>
+					<tr>
+						<td>{{ Form::label('company', 'Компания: ', ['class'=>'main_label']) }}</td>
+						<td>{{ Form::text('company', null, ['class'=>'change_input_contacts']) }}</td>
+					</tr>
+					<tr>
+						<td>{{ Form::label('phone', 'Контактный телефон: ', ['class'=>'main_label']) }}</td>
+						<td>{{ Form::text('phone', null, ['class'=>'change_input_contacts']) }}</td>
+					</tr>
+					<tr>
+						<td>{{ Form::label('email', 'E-mail: ', ['class'=>'main_label']) }}</td>
+						<td>{{ Form::text('email', null, ['class'=>'change_input_contacts', 'required']) }}</td>
+					</tr>
+					<tr>
+						<td>{{ Form::label('theme', 'Тема письма: ', ['class'=>'main_label']) }}</td>
+						<td>{{ Form::text('theme', null, ['class'=>'change_input_contacts']) }}</td>
+					</tr>
+					<tr>
+						<td>{{ Form::label('body', 'Текст: ', ['class'=>'main_label']) }}</td>
+						<td>{{ Form::textarea('body', null, ['class'=>'change_input_contacts contacts_form_body', 'required']) }}</td>
+					</tr>
+				</table>
+				{{ Form::submit('Отправить', ['class'=>'submit_field save_button']) }} 
+			{{ Form::close() }}
+		</div>
+		<div id="map-container"></div>
+		<script>    
           function init_map() {
             var var_location = new google.maps.LatLng(60.002168, 30.353743);
             var isDraggable = $(document).width() > 480 ? true : false;
@@ -86,7 +85,7 @@
             var var_marker = new google.maps.Marker({
                 position: var_location,
                 map: var_map,
-                title:"Gaudi"});
+                title:"ZIPO"});
      
             var var_map = new google.maps.Map(document.getElementById("map-container"),
                 var_mapoptions);
@@ -98,4 +97,5 @@
           google.maps.event.addDomListener(window, 'load', init_map);
      
         </script>
+	</div>
 @stop
