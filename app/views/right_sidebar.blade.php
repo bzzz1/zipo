de4f25f42d3d169ead4e557ce282d26a00f3f20d
Bogdan Kootz <bodyakootz@gmail.com>
2015-02-20 13:45:15 +0200

    sidebar_fix

--- app/views/right_sidebar.blade.php
+++ app/views/right_sidebar.blade.php
@@ -9,7 +9,18 @@
                             <img src="/img/photos/{{$article->photo}}" alt="{{$article->title}}" class="article_minimage">
                         </a>
           				<p class="article_date">{{$article->time}}</p>
-          				<a href='/articles/{{$HELP::url_slug(["$article->title"])}}?article_id={{$article->article_id}}' class="article_title">{{$article->title}}</a>
+                  @if (strLen($article->title)>=28)
+          				  <div class="article_title">
+                      <a href='/articles/{{$HELP::url_slug(["$article->title"])}}?article_id={{$article->article_id}}' class="article_title_1">{{mb_substr ($article->title, 0, 25)}} ...</a>
+                      <a href='/articles/{{$HELP::url_slug(["$article->title"])}}?article_id={{$article->article_id}}' class="article_title_1 article_title_full">{{$article->title}}</a>
+                    </div> 
+                  @else
+                    <div class="article_title">
+                      <a href='/articles/{{$HELP::url_slug(["$article->title"])}}?article_id={{$article->article_id}}' class="article_title_1">{{$article->title}}</a>
+                      <a href='/articles/{{$HELP::url_slug(["$article->title"])}}?article_id={{$article->article_id}}' class="article_title_1 article_title_full">{{$article->title}}</a>
+                    </div>  
+                  @endif 
+                  
           			</div>
                 @endforeach
       			<a href="/articles" class="articles_all_link">Все новости >> </a>
