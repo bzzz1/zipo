78526d346d448fcebefb60bc123acd95283a5faf
Iaroslav <beststrelok@gmail.com>
2015-02-24 17:22:11 +0200

    fix ckeditor

--- app/views/partials/admin_layout.blade.php
+++ app/views/partials/admin_layout.blade.php
@@ -21,25 +21,6 @@
 	{{ HTML::script('js/angular.min.js') }}
 	{{ HTML::script('js/jquery.min.js') }}
 	{{ HTML::script('ckeditor/ckeditor.js') }}
-	<script>
-		CKEDITOR.replace('ckeditor', {
-			filebrowserBrowseUrl 	   : '../kcfinder/browse.php?opener=ckeditor&type=files',
-			filebrowserImageBrowseUrl  : '../kcfinder/browse.php?opener=ckeditor&type=images',
-			filebrowserFlashBrowseUrl  : '../kcfinder/browse.php?opener=ckeditor&type=flash',
-			filebrowserUploadUrl  	   : '../kcfinder/upload.php?opener=ckeditor&type=files',
-			filebrowserImageUploadUrl  : '../kcfinder/upload.php?opener=ckeditor&type=images',
-			filebrowserFlashUploadUrl  : '../kcfinder/upload.php?opener=ckeditor&type=flash',
-			// uiColor: '#702329'
-			// toolbar : [
-				// ['ajaxsave'],
-				// ['Bold', 'Italic', 'Underline', '-', 'NumberedList', 'BulletedList', '-', 'Link', 'Unlink' ],
-				// ['Cut','Copy','Paste','PasteText'],
-				// ['Undo','Redo','-','RemoveFormat'],
-				// ['TextColor','BGColor'],
-				// ['Maximize', 'Image']
-			// ],
-		});
-	</script>
 	<!-- // <script src="vendor/ckeditor/ckeditor/ckeditor.js"></script> -->
 
 
@@ -63,6 +44,25 @@
 	</div>	
 	@yield('footer')
 
+	<script>
+		CKEDITOR.replace('ckeditor', {
+			filebrowserBrowseUrl 	   : '../kcfinder/browse.php?opener=ckeditor&type=files',
+			filebrowserImageBrowseUrl  : '../kcfinder/browse.php?opener=ckeditor&type=images',
+			filebrowserFlashBrowseUrl  : '../kcfinder/browse.php?opener=ckeditor&type=flash',
+			filebrowserUploadUrl  	   : '../kcfinder/upload.php?opener=ckeditor&type=files',
+			filebrowserImageUploadUrl  : '../kcfinder/upload.php?opener=ckeditor&type=images',
+			filebrowserFlashUploadUrl  : '../kcfinder/upload.php?opener=ckeditor&type=flash',
+			// uiColor: '#702329'
+			// toolbar : [
+				// ['ajaxsave'],
+				// ['Bold', 'Italic', 'Underline', '-', 'NumberedList', 'BulletedList', '-', 'Link', 'Unlink' ],
+				// ['Cut','Copy','Paste','PasteText'],
+				// ['Undo','Redo','-','RemoveFormat'],
+				// ['TextColor','BGColor'],
+				// ['Maximize', 'Image']
+			// ],
+		});
+	</script>
 	{{ HTML::script('js/jquery.columnizer.js') }}
 	{{ HTML::script('js/script.js') }}
 	{{ HTML::script('js/bootstrap.min.js') }}
