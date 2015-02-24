db0d34cd4927ee0b0c3521767d55d5241ea8efe4
alenasichkar <sichkar.alena@gmail.com>
2015-02-24 13:02:10 +0200

    items_sort form_control

--- app/views/items.blade.php
+++ app/views/items.blade.php
@@ -114,7 +114,7 @@
 			<div class="items_sort_by_main">
 				<p class="items sort_by">Показать по: </p>
 				<?php $q = http_build_query(Input::except(['page', 'pages_by'])); ?>
-				<select name="pages_by" id="pages_by" class="form-control">
+				<select name="pages_by" id="pages_by" class="form-control form_control">
 					<option data-link="{{ URL::current().'?'.$q.'&pages_by=10' }}">
 						10
 					</option>
