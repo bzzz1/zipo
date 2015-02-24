5f4a64eb36cfee84b5e68c91f951dfb97d377799
alenasichkar <sichkar.alena@gmail.com>
2015-02-24 20:17:10 +0200

    spec_flag hit_prodag

--- app/views/items.blade.php
+++ app/views/items.blade.php
@@ -53,64 +53,66 @@
 
 		</div>	
 		@foreach ($items as $item)
-			<div class="@if ($item->hit) item_hit @elseif ($item->special) item_spec @endif items_item_one">
+			<div class="empty_scape">
 				@if ($item->hit)
 					{{ HTML::image("img/markup/hit_prodag.png", "хит продаж", ['class'=>'items_item_flag']) }}
 				@elseif ($item->special)
 					{{ HTML::image("img/markup/spec_flag.png", "спецпредложение", ['class'=>'items_item_flag']) }}
 				@endif
-				<div class="items_item_text_block">	
-					<div class="items_item_heading">
-						<div class="name_and_code">
-							<div class="items_item_name_div">
-								<p class="items_item_name">{{$item->title}}</p>
-								<p class="items_item_name_full">{{$item->title}}</p>
+				<div class="@if ($item->hit) item_hit @elseif ($item->special) item_spec @endif items_item_one">
+					<div class="items_item_text_block">	
+						<div class="items_item_heading">
+							<div class="name_and_code">
+								<div class="items_item_name_div">
+									<p class="items_item_name">{{$item->title}}</p>
+									<p class="items_item_name_full">{{$item->title}}</p>
+								</div>	
 							</div>	
-						</div>	
-						<p class="items_item_code">Арт: {{$item->code}}</p>
-						<div class="items_item_price_div">
-							@if (Auth::user()->check())
-								<p class="items_item_price">{{$HELP::discount_price($item->price)}}&nbsp</p>
-							@else 
-								<p class="items_item_price">{{$item->price}}&nbsp</p>
-							@endif
-							<p class="items_item_currency">{{$item->currency}}.</p>
-						</div>	
-					</div>
-					<div class="items_item_descript">
-						{{ HTML::image("img/photos/$item->photo", "$item->title", ['class'=>'items_page_item_img']) }}
-						<table class="items_item_text">
-							<tr>
-								<td colspan='2' class="small_heading">Характеристики</td>
-							</tr>
-							<tr>
-								<td>Бренд:&nbsp&nbsp&nbsp&nbsp</td>
-								<td class="items_item_dyn_text">{{$item->producer}}</td>
-							</tr>
-							<tr>
-								<td>Код:</td>
-								<td class="items_item_dyn_text">{{$item->code}}</td>
-							</tr>
-							<tr>
-								<td>Тип:&nbsp</td>
-								<td class="items_item_dyn_text">{{$item->subcat}}</td>
-							</tr>
-							<tr>
-								<td>Наличие:&nbsp</td>
-								@if ($item->procurement)
-									<td>В наличии</td>
-								@else	
-									<td>Под заказ</td>
-								@endif	
-							</tr>
-						</table>
-					</div>
-				</div>	
-				<div class="items_buttons">
-			 		{{HTML::link($HELP::url_slug(["/", "$item->category", "/", "$item->subcat", "/", "$item->title"])."?subcat_id=$item->subcat_id&item_id=$item->item_id", 'Подробнее',['class'=>'btn btn-default items_button items_more']) }}
-					<a href="/order?item_id={{ $item->item_id }}" class="btn btn-default items_button items_order">Заказать</a>
-				</div>	
-			</div>
+							<p class="items_item_code">Арт: {{$item->code}}</p>
+							<div class="items_item_price_div">
+								@if (Auth::user()->check())
+									<p class="items_item_price">{{$HELP::discount_price($item->price)}}&nbsp</p>
+								@else 
+									<p class="items_item_price">{{$item->price}}&nbsp</p>
+								@endif
+								<p class="items_item_currency">{{$item->currency}}.</p>
+							</div>	
+						</div>
+						<div class="items_item_descript">
+							{{ HTML::image("img/photos/$item->photo", "$item->title", ['class'=>'items_page_item_img']) }}
+							<table class="items_item_text">
+								<tr>
+									<td colspan='2' class="small_heading">Характеристики</td>
+								</tr>
+								<tr>
+									<td>Бренд:&nbsp&nbsp&nbsp&nbsp</td>
+									<td class="items_item_dyn_text">{{$item->producer}}</td>
+								</tr>
+								<tr>
+									<td>Код:</td>
+									<td class="items_item_dyn_text">{{$item->code}}</td>
+								</tr>
+								<tr>
+									<td>Тип:&nbsp</td>
+									<td class="items_item_dyn_text">{{$item->subcat}}</td>
+								</tr>
+								<tr>
+									<td>Наличие:&nbsp</td>
+									@if ($item->procurement)
+										<td>В наличии</td>
+									@else	
+										<td>Под заказ</td>
+									@endif	
+								</tr>
+							</table>
+						</div>
+					</div>	
+					<div class="items_buttons">
+				 		{{HTML::link($HELP::url_slug(["/", "$item->category", "/", "$item->subcat", "/", "$item->title"])."?subcat_id=$item->subcat_id&item_id=$item->item_id", 'Подробнее',['class'=>'btn btn-default items_button items_more']) }}
+						<a href="/order?item_id={{ $item->item_id }}" class="btn btn-default items_button items_order">Заказать</a>
+					</div>	
+				</div>
+			</div>	
 		@endforeach
 		<div class="catalog_bottom_pages">
 			{{ $items->appends(Request::except('page'))->links('partials/zurb_presenter') }}
