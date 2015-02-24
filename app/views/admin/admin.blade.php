6c4229deca9e3b7d2c137b7d6ef0e70ef10ef3e8
Bogdan Kootz <bodyakootz@gmail.com>
2015-02-24 19:21:32 +0200

    admin_main_content_input

--- app/views/admin/admin.blade.php
+++ app/views/admin/admin.blade.php
@@ -15,9 +15,83 @@
 		<div class="admin_panel_import">
 			<p class="admin_uni_label"><i class="fa fa-reply"></i> Импорт</p>
 			{{ Form::open(['url'=>'/admin/import', 'files'=>true, 'method'=>'POST', 'class'=>'admin_panel_import']) }}
-				{{ Form::file('excel', ['class'=>'admin_uni_button']) }}
+				{{ Form::file('excel', ['class'=>'admin_panel_input']) }}
 				{{ Form::submit('Импортировать', ['class'=>'btn admin_uni_button']) }}
-			{{ Form::close() }}	
+			{{ Form::close() }}
+			<a href="" id="trigger_link" class="btn admin_uni_button">Выбрать файл</a>
+			<script>
+				$('#trigger_link').click(function(e){
+				    e.preventDefault();
+				    $('.admin_panel_input').trigger('click');
+				});
+				(function ( $, window, document, undefined ) {
+
+			    var defaults = {
+			        buttonText: "Browse",
+			        buttonClass: "btn admin_uni_button",
+			        selectedFileClass: 'selected-file',
+			        selectedFileEmptyText: ' No File',
+			    };
+
+			    function FilePrettify( element, options ) {
+			        this.element = element;
+			        this.$element = $(element);
+			        this.options = $.extend( {}, defaults, options) ;
+			        
+			        this._defaults = defaults;
+			        
+			        this.init();
+			    }
+
+			    FilePrettify.prototype.updateSelectedFile = function(fileName){
+			        if(!fileName)
+			            this.$selectedFile.html(' ' + this.options.selectedFileEmptyText);
+			        else
+			            this.$selectedFile.html(' ' + fileName.replace("C:\\fakepath\\", ""));
+			    }
+
+			    FilePrettify.prototype.init = function () {
+			        var self = this;
+			        var $elem = this.$element;
+			        $elem.hide();
+			        var $button = $('<button class = "' + this.options.buttonClass + '">' +
+			                this.options.buttonText + '</button>');
+			        var $selectedFile = $('<span class = "' + this.options.selectedFileClass 
+			                + '">' + this.options.selectedFileEmptyText + '</span>');
+			        $button.on('click.fileprettify', function(e){
+			            console.log($elem);
+			            e.preventDefault();
+			            $elem.click();
+			        });
+			        $($elem.parent()).append($button);
+			        $($elem.parent()).append($selectedFile);
+			        
+			        $elem.on('change.fileprettify', function(e){
+			            var fileName = $(this).val();
+			            self.updateSelectedFile(fileName);
+			        });
+
+			        this.$button = $button;
+			        this.$selectedFile = $selectedFile;
+			        this.updateSelectedFile($elem.val());
+			    };
+
+			    $.fn.fileprettify = function ( options ) {
+			        return this.each(function () {
+			            if (!$.data(this, 'fileprettify' )) {
+			                $.data(this, 'fileprettify' , 
+			                new FilePrettify( this, options ));
+			            }
+			        });
+			    }
+
+			})( jQuery, window, document )
+
+
+
+			$('input').fileprettify()
+
+			</script>	
 		</div>
 		<div class="admin_panel_search">
 			<p class="admin_uni_label"><i class="fa fa-search"></i> Поиск по артикулу</p>
--- app/views/partials/admin_sidebar.blade.php
+++ app/views/partials/admin_sidebar.blade.php
@@ -1,13 +1,13 @@
 @section ('left_sidebar')
 	<div class="full_screen">
 		<div class="admin_sidebar_container">
-			<a href="/admin" class="admin_sidebar_button"><i class="fa fa-wrench"></i>Панель управления</a>
-			<a href="/admin/change_item" class="admin_sidebar_button"><i class="fa fa-cart-plus"></i>Добавить товар</a>
-			<a href="/admin/change_article" class="admin_sidebar_button"><i class="fa fa-bullhorn"></i>Добавить новость</a>
-			<a href="/admin/subcats" class="admin_sidebar_button"><i class="fa fa-sitemap"></i>Подкатегории</a>
-			<a href="/admin/catalog" class="admin_sidebar_button"><i class="fa fa-book"></i>Каталог</a>
-			<a href="/admin/articles" class="admin_sidebar_button"><i class="fa fa-list-alt"></i>Список новостей</a>
-			<a href="/admin/producers" class="admin_sidebar_button"><i class="fa fa-users"></i>Производители</a>
+			<a href="/admin" class="@if ( $env == 'panel') active_my @endif admin_sidebar_button"><i class="fa fa-wrench"></i>Панель управления</a>
+			<a href="/admin/change_item" class="@if ($env == 'change_item') active_my @endif admin_sidebar_button"><i class="fa fa-cart-plus"></i>Добавить товар</a>
+			<a href="/admin/change_article" class="@if ($env == 'change_article') active_my @endif admin_sidebar_button"><i class="fa fa-bullhorn"></i>Добавить новость</a>
+			<a href="/admin/subcats" class="@if ($env == 'subcats') active_my @endif admin_sidebar_button"><i class="fa fa-sitemap"></i>Подкатегории</a>
+			<a href="/admin/catalog" class="@if ($env == 'catalog_admin') active_my @endif admin_sidebar_button"><i class="fa fa-book"></i>Каталог</a>
+			<a href="/admin/articles" class="@if ($env == 'articles') active_my @endif admin_sidebar_button"><i class="fa fa-list-alt"></i>Список новостей</a>
+			<a href="/admin/producers" class="@if ($env == 'producers') active_my @endif admin_sidebar_button"><i class="fa fa-users"></i>Производители</a>
 		</div>
 	</div>	
 @stop
