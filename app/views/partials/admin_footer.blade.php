1c1c31efddabd8f86863d6cf2743dbec76e62086
Bogdan Kootz <bodyakootz@gmail.com>
2015-02-25 09:48:37 +0200

    admin_footer

--- app/views/partials/admin_footer.blade.php
+++ app/views/partials/admin_footer.blade.php
@@ -2,7 +2,7 @@
 	<footer>
 		<p class="copy_footer_admin">made by&nbsp 
 			<a href="http://www.dev.bzzz.biz.ua">
-				<img src="img/markup/our_logo_footer.png" alt="bzzz web development studio logo">
+				<img src="../../img/markup/our_logo_footer.png" alt="bzzz web development studio logo">
 			</a>
 		</p>
 	</footer>	
