d0d1fd3046f15e4fbf0c996a4f32fe94506ad8e8
Bogdan Kootz <bodyakootz@gmail.com>
2015-02-25 18:58:42 +0200

    admin_subcats_empy_block_fix

--- app/views/admin/admin_subcats.blade.php
+++ app/views/admin/admin_subcats.blade.php
@@ -9,45 +9,48 @@
 		<div class="admin_catalog_category" data-category='Механическое_en'>
 			<h4 class="admin_one_cat_heading">Механическое <br> оборудование(импортное)</h4>
 			<a href="/admin/change_subcat" class="admin_one_cat_add"><i class="fa fa-plus">&nbsp</i>Добавить подкатегорию</a>
-			<div class="admin_one_cat_subcats_block">
-				<div class="admin_subcats_list">
-					<div class="subcategory_left">
-						<ul>
-							@foreach ($HELP::columnize($subcats['Механическое_en'], 2, 1) as $subcat)
-								<li>
-									<p class="admin_subcategory">
-										{{ $subcat->subcat }}
-										<a href="/admin/change_subcat?subcat_id={{$subcat->subcat_id}}"><i class="fa fa-pencil change_icon"></i></a>
-										{{ Form::open(array('url' => "/admin/delete_subcat?subcat_id=$subcat->subcat_id", 'method' => 'POST', 'class'=>'admin_subcategory_form')) }}
-											<i class="fa fa-times delete_icon"></i>
-										{{ Form::close() }} 
-									</p>
-								</li>
-							@endforeach
-						</ul>
-					</div>
-					<div class="subcategory_right">
-						<ul>	
-							@foreach ($HELP::columnize($subcats['Механическое_en'], 2, 2) as $subcat)
-								<li>
-									<p class="admin_subcategory">
-										{{ $subcat->subcat }}
-										<a href="/admin/change_subcat?subcat_id={{$subcat->subcat_id}}"><i class="fa fa-pencil change_icon"></i></a>
-										{{ Form::open(array('url' => "/admin/delete_subcat?subcat_id=$subcat->subcat_id", 'method' => 'POST', 'class'=>'admin_subcategory_form')) }}
-											<i class="fa fa-times delete_icon"></i>
-										{{ Form::close() }} 
-									</p>
-								</li>
-							@endforeach
-						</ul>	
-					</div>	
-				</div><!-- admin_subcats_list -->
-			</div><!--admin_one_cat_subcats_block-->
+			@if (count($subcats['Механическое_en']))
+				<div class="admin_one_cat_subcats_block">
+					<div class="admin_subcats_list">
+						<div class="subcategory_left">
+							<ul>
+								@foreach ($HELP::columnize($subcats['Механическое_en'], 2, 1) as $subcat)
+									<li>
+										<p class="admin_subcategory">
+											{{ $subcat->subcat }}
+											<a href="/admin/change_subcat?subcat_id={{$subcat->subcat_id}}"><i class="fa fa-pencil change_icon"></i></a>
+											{{ Form::open(array('url' => "/admin/delete_subcat?subcat_id=$subcat->subcat_id", 'method' => 'POST', 'class'=>'admin_subcategory_form')) }}
+												<i class="fa fa-times delete_icon"></i>
+											{{ Form::close() }} 
+										</p>
+									</li>
+								@endforeach
+							</ul>
+						</div>
+						<div class="subcategory_right">
+							<ul>	
+								@foreach ($HELP::columnize($subcats['Механическое_en'], 2, 2) as $subcat)
+									<li>
+										<p class="admin_subcategory">
+											{{ $subcat->subcat }}
+											<a href="/admin/change_subcat?subcat_id={{$subcat->subcat_id}}"><i class="fa fa-pencil change_icon"></i></a>
+											{{ Form::open(array('url' => "/admin/delete_subcat?subcat_id=$subcat->subcat_id", 'method' => 'POST', 'class'=>'admin_subcategory_form')) }}
+												<i class="fa fa-times delete_icon"></i>
+											{{ Form::close() }} 
+										</p>
+									</li>
+								@endforeach
+							</ul>	
+						</div>	
+					</div><!-- admin_subcats_list -->
+				</div><!--admin_one_cat_subcats_block-->
+			@endif
 		</div>	
 		<div class="admin_catalog_category" data-category='Тепловое_en'>
 			<h4 class="admin_one_cat_heading">Тепловое <br> оборудование(импортное)</h4>
 			<a href="/admin/change_subcat" class="admin_one_cat_add"><i class="fa fa-plus">&nbsp</i>Добавить подкатегорию</a>
-			<div class="admin_one_cat_subcats_block">
+			@if (count($subcats['Тепловое_en']))
+				<div class="admin_one_cat_subcats_block">
 				<div class="admin_subcats_list">
 					<div class="subcategory_left">
 						<ul>
@@ -80,12 +83,14 @@
 						</ul>	
 					</div>
 				</div><!-- admin_subcats_list -->		
-			</div><!--admin_one_cat_subcats_block-->
+				</div><!--admin_one_cat_subcats_block-->
+			@endif
 		</div>
 		<div class="admin_catalog_category" data-category='Холодильное_en'>
 			<h4 class="admin_one_cat_heading">Холодильное <br> оборудование(импортное)</h4>
 			<a href="/admin/change_subcat" class="admin_one_cat_add"><i class="fa fa-plus">&nbsp</i>Добавить подкатегорию</a>
-			<div class="admin_one_cat_subcats_block">
+			@if (count($subcats['Холодильное_en']))
+				<div class="admin_one_cat_subcats_block">
 				<div class="admin_subcats_list">
 					<div class="subcategory_left">
 							<ul>
@@ -118,12 +123,14 @@
 							</ul>	
 					</div>	
 				</div><!-- admin_subcats_list -->	
-			</div><!--admin_one_cat_subcats_block-->
+				</div><!--admin_one_cat_subcats_block-->
+			@endif
 		</div>
 		<div class="admin_catalog_category posud_catedory" data-category='Посудомоечное_en'>
 			<h4 class="admin_one_cat_heading">Посудомоечное <br> оборудование(импортное)</h4>
 			<a href="/admin/change_subcat" class="admin_one_cat_add"><i class="fa fa-plus">&nbsp</i>Добавить подкатегорию</a>
-			<div class="admin_one_cat_subcats_block">
+			@if (count($subcats['Посудомоечное_en']))
+				<div class="admin_one_cat_subcats_block">
 				<div class="admin_subcats_list">
 					<div class="subcategory_left">
 							<ul>
@@ -156,50 +163,54 @@
 							</ul>	
 					</div>	
 				</div><!-- admin_subcats_list -->
-			</div><!--admin_one_cat_subcats_block-->
+				</div><!--admin_one_cat_subcats_block-->
+			@endif	
 		</div>
 		<div class="admin_catalog_category" data-category='Механическое_ru'>
 			<h4 class="admin_one_cat_heading">Механическое <br> оборудование(российское)</h4>
 			<a href="/admin/change_subcat" class="admin_one_cat_add"><i class="fa fa-plus">&nbsp</i>Добавить подкатегорию</a>
-			<div class="admin_one_cat_subcats_block">
-				<div class="admin_subcats_list">
-					<div class="subcategory_left">
-							<ul>
-								@foreach ($HELP::columnize($subcats['Механическое_ru'], 2, 1) as $subcat)
-									<li>
-										<p class="admin_subcategory">
-											{{ $subcat->subcat }}
-											<a href="/admin/change_subcat?subcat_id={{$subcat->subcat_id}}"><i class="fa fa-pencil change_icon"></i></a>
-											{{ Form::open(array('url' => "/admin/delete_subcat?subcat_id=$subcat->subcat_id", 'method' => 'POST', 'class'=>'admin_subcategory_form')) }}
-												<i class="fa fa-times delete_icon"></i>
-											{{ Form::close() }} 
-										</p>
-									</li>
-								@endforeach
-							</ul>
-					</div>
-					<div class="subcategory_right">
-							<ul>	
-								@foreach ($HELP::columnize($subcats['Механическое_ru'], 2, 2) as $subcat)
-									<li>
-										<p class="admin_subcategory">
-											{{ $subcat->subcat }}
-											<a href="/admin/change_subcat?subcat_id={{$subcat->subcat_id}}"><i class="fa fa-pencil change_icon"></i></a>
-											{{ Form::open(array('url' => "/admin/delete_subcat?subcat_id=$subcat->subcat_id", 'method' => 'POST', 'class'=>'admin_subcategory_form')) }}
-												<i class="fa fa-times delete_icon"></i>
-											{{ Form::close() }} 
-										</p>
-									</li>
-								@endforeach
-							</ul>	
-					</div>
-				</div><!-- admin_subcats_list -->	
-			</div><!--admin_one_cat_subcats_block-->
+			@if (count($subcats['Механическое_ru']))
+				<div class="admin_one_cat_subcats_block">
+					<div class="admin_subcats_list">
+						<div class="subcategory_left">
+								<ul>
+									@foreach ($HELP::columnize($subcats['Механическое_ru'], 2, 1) as $subcat)
+										<li>
+											<p class="admin_subcategory">
+												{{ $subcat->subcat }}
+												<a href="/admin/change_subcat?subcat_id={{$subcat->subcat_id}}"><i class="fa fa-pencil change_icon"></i></a>
+												{{ Form::open(array('url' => "/admin/delete_subcat?subcat_id=$subcat->subcat_id", 'method' => 'POST', 'class'=>'admin_subcategory_form')) }}
+													<i class="fa fa-times delete_icon"></i>
+												{{ Form::close() }} 
+											</p>
+										</li>
+									@endforeach
+								</ul>
+						</div>
+						<div class="subcategory_right">
+								<ul>	
+									@foreach ($HELP::columnize($subcats['Механическое_ru'], 2, 2) as $subcat)
+										<li>
+											<p class="admin_subcategory">
+												{{ $subcat->subcat }}
+												<a href="/admin/change_subcat?subcat_id={{$subcat->subcat_id}}"><i class="fa fa-pencil change_icon"></i></a>
+												{{ Form::open(array('url' => "/admin/delete_subcat?subcat_id=$subcat->subcat_id", 'method' => 'POST', 'class'=>'admin_subcategory_form')) }}
+													<i class="fa fa-times delete_icon"></i>
+												{{ Form::close() }} 
+											</p>
+										</li>
+									@endforeach
+								</ul>	
+						</div>
+					</div><!-- admin_subcats_list -->	
+				</div><!--admin_one_cat_subcats_block-->
+			@endif	
 		</div>
 		<div class="admin_catalog_category" data-category='Тепловое_ru'>
 			<h4 class="admin_one_cat_heading">Тепловое <br> оборудование(российское)</h4>
 			<a href="/admin/change_subcat" class="admin_one_cat_add"><i class="fa fa-plus">&nbsp</i>Добавить подкатегорию</a>
-			<div class="admin_one_cat_subcats_block">
+			@if (count($subcats['Тепловое_ru']))
+				<div class="admin_one_cat_subcats_block">
 				<div class="admin_subcats_list">
 					<div class="subcategory_left">
 							<ul>
@@ -232,12 +243,14 @@
 						</ul>	
 					</div>	
 				</div><!-- admin_subcats_list -->
-			</div><!--admin_one_cat_subcats_block-->
+				</div><!--admin_one_cat_subcats_block-->
+			@endif	
 		</div>
 		<div class="admin_catalog_category" data-category='Холодильное_ru'>
 			<h4 class="admin_one_cat_heading">Холодильное <br> оборудование(российское)</h4>
 			<a href="/admin/change_subcat" class="admin_one_cat_add"><i class="fa fa-plus">&nbsp</i>Добавить подкатегорию</a>
-			<div class="admin_one_cat_subcats_block">
+			@if (count($subcats['Холодильное_ru']))
+				<div class="admin_one_cat_subcats_block">
 				<div class="admin_subcats_list">
 					<div class="subcategory_left">
 							<ul>
@@ -270,12 +283,14 @@
 						</ul>	
 					</div>
 				</div><!-- admin_subcats_list -->	
-			</div><!--admin_one_cat_subcats_block-->
+				</div><!--admin_one_cat_subcats_block-->
+			@endif	
 		</div>
 		<div class="admin_catalog_category posud_catedory" data-category='Посудомоечное_ru'>
 			<h4 class="admin_one_cat_heading">Посудомоечное <br> оборудование(российское)</h4>
 			<a href="/admin/change_subcat" class="admin_one_cat_add"><i class="fa fa-plus">&nbsp</i>Добавить подкатегорию</a>
-			<div class="admin_one_cat_subcats_block">
+			@if (count($subcats['Посудомоечное_ru']))
+				<div class="admin_one_cat_subcats_block">
 				<div class="admin_subcats_list">
 					<div class="subcategory_left">
 						<ul>
@@ -308,7 +323,8 @@
 						</ul>	
 					</div>
 				</div><!-- admin_subcats_list -->
-			</div><!--admin_one_cat_subcats_block-->
+				</div><!--admin_one_cat_subcats_block-->
+			@endif	
 		</div>
 	</div>
 @stop
