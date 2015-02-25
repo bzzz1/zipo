57ce4eb91be69e9bf0e73fce492fe4e7e7351a75
alenasichkar <sichkar.alena@gmail.com>
2015-02-25 10:43:39 +0200

    admin_change_article

--- app/views/admin/admin_change_article.blade.php
+++ app/views/admin/admin_change_article.blade.php
@@ -9,12 +9,12 @@
 		{{ Form::model($article, ['url'=>['/admin/update_article'], 'method'=>'POST', 'class'=>'']) }}
 			<div class="change_article_title_block">
  				{{ Form::label('title', 'Заголовок: ', ['class'=>'admin_uni_label']) }}
-				{{ Form::text('title', null, ['class'=>'form-control', 'required']) }}
+				{{ Form::text('title', null, ['class'=>'form-control change_article_title_form', 'required']) }}
 				<a href="/admin/articles" class="change_adrticle_to_all"><i class="fa fa-list-alt"></i>&nbsp К списку новостей</a>
 			</div>
 			<div class="change_article_weight_div">
  				{{ Form::label('weight', 'Вес новости: ', ['class'=>'admin_uni_label']) }}
-				{{ Form::text('weight', null, ['class'=>'form-control', 'required']) }}
+				{{ Form::text('weight', null, ['class'=>'form-control change_article_weight_form', 'required']) }}
 			</div>
 			<div class="change_article_descript_block">
 				{{ Form::textarea('body', null, array('class' => 'name', 'id' => 'ckeditor')) }}
@@ -28,17 +28,18 @@
 				@endif	
 			</div>
 			<div class="change_item_buttons">
-				<a href="#" class="admin_uni_button">Очистить</a>
+				<a href="#" class="btn admin_uni_button">Очистить</a>
 			</div>
-			{{ Form::submit('Сохранить', ['class'=>'admin_uni_button']) }}
+			{{ Form::submit('Сохранить', ['class'=>'btn admin_uni_button']) }}
 		{{ Form::close() }}
 
 		<div class="change_article_img">
 			<p class="admin_uni_label">Добавить миниатюру для статьи</p>
 			{{ Form::open(['url'=>'/admin/article_upload_image', 'files'=>true, 'method'=>'POST', 'class'=>'admin_panel_import']) }}
-				{{ Form::file('photo', ['class'=>'admin_uni_button']) }}
+				{{ Form::file('photo', ['class'=>'']) }}
 				{{ Form::submit('Добавить', ['class'=>'btn admin_uni_button']) }}
 			{{ Form::close() }}
+			<a href="" id="trigger_link" class="btn admin_uni_button">Выбрать файл</a>
 		</div>
 
 		@if ($article)
