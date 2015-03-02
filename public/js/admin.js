ba7e3800840a56c1dacb2342662a584622427a46
Bogdan Kootz <bodyakootz@gmail.com>
2015-03-02 21:05:43 +0200

    lins_fix

--- public/js/admin.js
+++ public/js/admin.js
@@ -81,8 +81,17 @@ $('.delete_items_group_icon').on('click', function() {
 $('.clear_item_button').on('click', function() {
 	var $form = $('.update_item_form'); 
 	$form.find('input[name="title"]').val("");
-	
-
+	$form.find('input[name="code"]').val("");
+	$form.find('select[name="category"]').val("Механическое_en");
+	send_category("Механическое_en");
+	var first_p = $form.find('select[name="producer_id"] option').eq(0).val();
+	$form.find('select[name="producer_id"]').val(first_p);
+	$form.find('input[name="price"]').val("");
+	$form.find('input[name="currency"]').val("");
+	$form.find('checkbox[name="procurement"]').val(true);
+	$form.find('input[name="description"]').val("");
+	$form.find('checkbox[name="special"]').val(false);
+	$form.find('checkbox[name="hit"]').val(false);
 	// $input = 
 	// $('input[name="title"]')
 	// $('input[name="code"]')
