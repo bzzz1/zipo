0d218997753ec04b4d44a245630dbc5d2ec80e25
Bogdan Kootz <bodyakootz@gmail.com>
2015-04-23 18:09:34 +0300

    delete_pdf_0.9

---
+++ app/views/admin/admin_pdfs.blade.php
@@ -0,0 +1,20 @@
+ @extends('partials/admin_layout')
+@extends('partials/admin_header')
+@extends('partials/admin_sidebar')
+@extends('partials/admin_footer')
+
+@section('body')
+	<div class="admin_main_content">
+		<ul class="all_pdfs">
+			@foreach ($pdfs as $pdf)
+				<li>
+					{{ Form::open(array('url' => "/admin/delete_pdf?pdf_id=$pdf->pdf_id", 'method' => 'POST', 'class'=>'admin_del_pdf_form ')) }}
+						<i class="fa fa-times del_pdf"></i>
+						{{ Form::submit('Сохранить', ['class'=>'hidden']) }}
+					{{ Form::close() }} 
+					{{HTML::link("/pdf/$pdf->file", $pdf->good,['target'=>'_blank']) }}
+				</li>
+			@endforeach		
+		</ul>
+	</div>
+@stop
