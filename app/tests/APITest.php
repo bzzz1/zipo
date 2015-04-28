f7744ce26b0f5a08995b9fa4fc9429ac8df929c3
Iaroslav <beststrelok@gmail.com>
2015-04-28 15:38:18 +0300

    add tests

---
+++ app/tests/APITest.php
@@ -0,0 +1,29 @@
+<?php
+
+class APITest extends TestCase {
+
+	public function testIndex()
+	{
+		$response = $this->call('GET', '/');
+		$this->data = $response->original->getData();
+
+		$this->assertType('articles', 'Illuminate\Database\Eloquent\Collection');
+		$this->assertType('recents', 'array');
+		$this->assertType('producers', 'Illuminate\Database\Eloquent\Collection');
+		$this->assertType('subcats', 'Subcat');
+		$this->assertType('env', 'string');
+	}
+
+	public function testItems() 
+	{
+		$response = $this->action('GET', 'MainController@items', ['category'=>'1', 'subcat'=>'1', 'subcat_id'=>'1']);
+		$this->data = $response->original->getData();
+
+		$this->assertType('items', 'Illuminate\Pagination\Paginator');
+		$this->assertType('current', 'Subcat');
+		$this->assertType('articles', 'Illuminate\Database\Eloquent\Collection');
+		$this->assertType('recents', 'array');
+		$this->assertType('producers', 'Illuminate\Database\Eloquent\Collection');
+		$this->assertType('env', 'string');
+	}
+}
