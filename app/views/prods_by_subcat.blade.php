15acf9e09f6d2793780f489db7968159e33d3e93
Bogdan Kootz <bodyakootz@gmail.com>
2015-05-08 15:06:47 +0300

    ppod_by_subcat_2

--- app/views/prods_by_subcat.blade.php
+++ app/views/prods_by_subcat.blade.php
@@ -26,44 +26,17 @@
 				<option data-link="{{URL::current().'?'.$q.'&sort=title&order=desc' }}">
 					имени(я-а)
 				</option>
-				<option data-link="{{URL::current().'?'.$q.'&sort=price&order=asc' }}">
-					цене($-$$$)
-				</option>
-				<option data-link="{{URL::current().'?'.$q.'&sort=price&order=desc' }}">
-					цене($$$-$)
-				</option>
 			</select>
 
 		</div>	
 		<div class="empty_scape">
 			<ul>
-				@foreach ($produsers as $producer)
+				@foreach ($producers as $producer)
 					<li>
-				 		{{HTML::link($HELP::url_slug(["/", "$current->category", "/", "$current->subcat", "/", "$producer->producer"])."?subcat_id=$current->subcat_id&prod_id=$producer->producer_id", $producer->producer,['class'=>'prod_by_subcat_one']) }}
+				 		{{HTML::link($HELP::url_slug(["/", "$current->category", "/", "$current->subcat", "/", "$producer->producer", "/", "items"])."?subcat_id=$current->subcat_id&producer_id=$producer->producer_id", $producer->producer,['class'=>'prod_by_subcat_one']) }}
 					</li>
 				@endforeach
 			</ul>
-		</div>
-		<div class="catalog_bottom_pages">
-			{{ $items->appends(Request::except('page'))->links('partials/zurb_presenter') }}
-			<div class="items_sort_by_main">
-				<p class="items sort_by">Показать по: </p>
-				<?php $q = http_build_query(Input::except(['page', 'pages_by'])); ?>
-				<select name="pages_by" id="pages_by" class="form-control form_control">
-					<option data-link="{{ URL::current().'?'.$q.'&pages_by=10' }}">
-						10
-					</option>
-					<option data-link="{{ URL::current().'?'.$q.'&pages_by=50' }}">
-						50
-					</option>
-					<option data-link="{{ URL::current().'?'.$q.'&pages_by=100' }}">
-						100
-					</option>
-					<option data-link="{{ URL::current().'?'.$q.'&pages_by=1000000' }}">
-						все
-					</option>
-				</select>
-			</div>
 		</div>
 	</div>	
 @stop
